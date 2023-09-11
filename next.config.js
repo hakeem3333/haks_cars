cf1b01b426fc28ff61eaea426eb38092d5431f55
hakeem3333 <hakeemsalaudeen460@gmail.com>
2023-09-11 12:35:55 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+// module.exports = {
+//     images: {
+//       domains: ['cdn.imagin.studio'],
+//     },
+//   }
+  
+const nextConfig = {
+    images: {
+      domains: ['cdn.imagin.studio'],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
